37553666e3646df74a4e00a180653d962c98d53b
pi <pi@raspberrypi>
2017-04-05 13:26:07 +0000

    mi band step counter test

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,51 @@
 var express = require('express');
 var router = express.Router();
 
+var noble = require('noble'); // https://github.com/sandeepmistry/noble/wiki/Getting-started
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+noble.startScanning();
+
+	noble.on('stateChange', function(state) {
+	  if (state === 'poweredOn') {
+	    noble.startScanning();
+	  } else {
+	    noble.stopScanning();
+	  }
+	});
+
+	noble.on('discover', function(peripheral) {
+	    console.log('Found device with local name: ' + peripheral.advertisement.localName);
+	    console.log('advertising the following service uuid\'s: ' + peripheral.advertisement.serviceUuids);
+	    console.log();
+		if(peripheral.advertisement.localName=="MI1S"){
+
+		peripheral.connect(function(error) {
+		    console.log('connected to peripheral: ' + peripheral.uuid);
+		    peripheral.discoverServices(['fee0'], function(error, services) {
+		      var deviceInformationService = services[0];
+		      console.log('discovered device information service');
+
+		      deviceInformationService.discoverCharacteristics(['ff06'], function(error, characteristics) {
+			var manufacturerNameCharacteristic = characteristics[0];
+			console.log('discovered manufacturer name characteristic');
+
+			manufacturerNameCharacteristic.read(function(error, data) {
+			  // data is a buffer
+			  //console.log('manufacture name is: ' + data.toString('utf8'));
+				console.log('data read: ' + data.toString('utf8'));
+				dataUint8Array = new Uint8Array(data);
+				console.log('steps: ' + dataUint8Array[0] |  (dataUint8Array[1] << 8) |  (dataUint8Array[2] << 16) |  (dataUint8Array[3] << 24));
+			});
+		      });
+		    });
+		  });
+
+		}
+	});
+
   res.render('index', { title: 'Express' });
 });
 
