5a9170af898109b4100285722d128d2e783840f3
pi <pi@raspberrypi>
2017-04-05 15:22:02 +0000

    miband rssi and battery level

--- routes/index.js
+++ routes/index.js
@@ -2,47 +2,62 @@ var express = require('express');
 var router = express.Router();
 
 var noble = require('noble'); // https://github.com/sandeepmistry/noble/wiki/Getting-started
+var REALTIME_STEPS_UUID = 'ff06';
+var SERVICE_UUID = 'fee0';
+var DEVICE_NAME = 'MI1S';
+var BATTERY_INFO_UUID = 'ff0c';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-noble.startScanning();
+	noble.startScanning();
 
 	noble.on('stateChange', function(state) {
-	  if (state === 'poweredOn') {
-	    noble.startScanning();
-	  } else {
-	    noble.stopScanning();
-	  }
+		if (state === 'poweredOn') {
+		    	noble.startScanning();
+		} else {
+			noble.stopScanning();
+		}
 	});
 
 	noble.on('discover', function(peripheral) {
-	    console.log('Found device with local name: ' + peripheral.advertisement.localName);
-	    console.log('advertising the following service uuid\'s: ' + peripheral.advertisement.serviceUuids);
-	    console.log();
-		if(peripheral.advertisement.localName=="MI1S"){
+		console.log('Found device with local name: ' + peripheral.advertisement.localName);
+		console.log('advertising the following service uuid\'s: ' + peripheral.advertisement.serviceUuids);
+		console.log();
+		
+		if(peripheral.advertisement.localName==DEVICE_NAME){
+			console.log('device RSSI information: ' + peripheral.rssi + 'db');
+			peripheral.connect(function(error) {
+				console.log('connected to peripheral: ' + peripheral.uuid);
 
-		peripheral.connect(function(error) {
-		    console.log('connected to peripheral: ' + peripheral.uuid);
-		    peripheral.discoverServices(['fee0'], function(error, services) {
-		      var deviceInformationService = services[0];
-		      console.log('discovered device information service');
+				peripheral.discoverServices([SERVICE_UUID], function(error, services) {
+					var deviceInformationService = services[0];
+					console.log('discovered device information service');
 
-		      deviceInformationService.discoverCharacteristics(['ff06'], function(error, characteristics) {
-			var manufacturerNameCharacteristic = characteristics[0];
-			console.log('discovered manufacturer name characteristic');
+					deviceInformationService.discoverCharacteristics([REALTIME_STEPS_UUID ], function(error, characteristics) {
+						var realtimeStepsCharacteristic = characteristics[0];
+						console.log('discovered realtime steps characteristic');
 
-			manufacturerNameCharacteristic.read(function(error, data) {
-			  // data is a buffer
-			  //console.log('manufacture name is: ' + data.toString('utf8'));
-				console.log('data read: ' + data.toString('utf8'));
-				dataUint8Array = new Uint8Array(data);
-				console.log('steps: ' + dataUint8Array[0] |  (dataUint8Array[1] << 8) |  (dataUint8Array[2] << 16) |  (dataUint8Array[3] << 24));
+						realtimeStepsCharacteristic.read(function(error, data) {
+							var steps = (data[3] << 24) | (data[2] << 16) | (data[1] << 8) | data[0];
+							console.log('Steps Counter: ' + steps);
+						});
+					});
+
+					deviceInformationService.discoverCharacteristics([BATTERY_INFO_UUID ], function(error, characteristics) {
+						var batteryInfoCharacteristic = characteristics[0];
+						console.log('discovered battery characteristic');
+
+						batteryInfoCharacteristic.read(function(error, data) {
+							if(error){
+								console.error(error);
+							}
+							var batteryLevel = data[0];
+						});
+					});
+
+				});
 			});
-		      });
-		    });
-		  });
-
 		}
 	});
 
