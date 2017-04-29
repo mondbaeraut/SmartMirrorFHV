2347c2f7adf0dd6d36c9364a198a93d02969f4bb
daniel <dgr7348@students.fhv.at>
2017-04-29 19:12:52 +0200

    save daily steps data

--- routes/apiMiBand.js
+++ routes/apiMiBand.js
@@ -1,16 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var getMiBandData = require('../miband/miband');
+var miBand = require('../miband/miband');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-
-	getMiBandData.then(function (response) {
+	miBand.getMiBandData().then(function (response) {
 		console.log("Success!", response.steps);
-        res.json({uuid: response.uuid, steps: response.steps, rssi: response.rssi}); 
+		res.json({ uuid: response.uuid, steps: response.steps, rssi: response.rssi });
 	}, function (error) {
 		console.error("Failed!", error);
-        res.json(error); 
+		res.json(error);
 	});
 });
 
