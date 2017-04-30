e76b688b3ae2add074e4f8ef2eccfb97d9f9e295
nfe3303@students.fhv.at <nfe3303@students.fhv.at>
2017-04-30 16:10:51 +0200

    Layout changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var apiMiBand = require('./routes/apiMiBand');
+//var apiMiBand = require('./routes/apiMiBand');
 
 var app = express();
 
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/api/miband', apiMiBand);
+//app.use('/api/miband', apiMiBand);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- public/javascript/miband.js
+++ public/javascript/miband.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $.ajax({
         url: "/api/miband"
     }).then(function(data) {
-        $('#steps').append(data.steps + ' Schritte');
+        //$('#steps').append(data.steps + ' Schritte');
+        $('#steps').append("10000 " + ' Schritte');
     });
 });
