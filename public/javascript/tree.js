fab605fcc138109cb826ae32ff747d34cce76c2e
benimurza <benimurza@yahoo.com>
2017-05-01 18:35:34 +0200

    Added leaf animation trigger

--- public/javascript/tree.js
+++ public/javascript/tree.js
@@ -1,19 +1,55 @@
-var i = 0;
 
-var container = document.getElementById("container");
+// Maximum number of leaves
+var maxLeaves = 5;
+var leafCounter = 0;
+
+var container = document.getElementById("right");
+
+var leafOnTree = 0;
+// Helper function for adding leaves to the tree
 container.addEventListener("click", function(e) {
-	console.log(`#leaf${i++} { position: absolute;
-	left: ${e.clientX-this.offsetLeft}px;
-	top: ${e.clientY-this.offsetTop-11}px; 
+	console.log(`#leaf${leafOnTree++} { position: absolute;
+	left: ${(e.clientX-11)*100/1600}%;
+	top: ${(e.clientY-11)*100/900}%; 
 	width:11px; 
 	height:11px;
 	animation-name: example;
     animation-duration: 8s;
 	animation-fill-mode: forwards;}`);
 });
-for(var i=0;i<17;i++){
+
+// Create all leaf objects and "place" them on the tree
+for(var leafOnTree=0;leafOnTree<maxLeaves;leafOnTree++){
 	var image = document.createElement("img");
-	//image.src = "/resources/leaf2.svg";
-	image.id = `leaf${i}`;
+	image.src = "/resources/leaf2.svg";
+	image.id = `leaf${leafOnTree}`;
 	container.append(image);
 }
+
+// Add animation to leaf
+function triggerAnimationOnLeaf(leafId){
+	document.getElementById(leafId).classList.add('leafFallAnimation');
+}
+
+// Remove animation from leaf, setting it to the initial position
+function resetLeaf(leafId){
+	document.getElementById(leafId).classList.remove('leafFallAnimation');
+}
+
+// Reset all leaves at a specified interval
+setInterval( function() {
+	for (var i = 0; i < maxLeaves; i++){
+		resetLeaf("leaf"+i);
+	}
+	leafCounter = 0;
+}, 35000 );
+
+function getNextLeaf() {
+	if(leafCounter == maxLeaves) {
+		// All leaves have fallen
+		return null;
+	}
+	return "leaf"+leafCounter++;
+}
+
+setInterval( function() {triggerAnimationOnLeaf(getNextLeaf())}, 5000);
