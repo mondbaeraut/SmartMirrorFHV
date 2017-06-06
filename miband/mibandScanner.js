801c30f2c3d17f7265fafcc5e266cfe3af21178c
Arno Riedmann <arno.riedmann@gmx.at>
2017-06-06 16:27:55 +0200

    noble workaround

--- miband/mibandScanner.js
+++ miband/mibandScanner.js
@@ -1,5 +1,9 @@
 // Modules
-var noble = require('noble'); // https://github.com/sandeepmistry/noble/wiki/Getting-started
+try {
+    var noble = require('noble'); // https://github.com/sandeepmistry/noble/wiki/Getting-started
+} catch (e) {
+    console.error("Just run npm install lul");
+}
 var database = require('./database');
 
 // Constants
@@ -11,7 +15,7 @@ var BATTERY_INFO_UUID = 'ff0c';
 var RSSI_THRESHOLD = -70;
 
 // load values via bluetooth
-module.exports.startScanning = function () {
+module.exports.startScanning = !noble ? function () {} : function () {
     // start scanning when function is called
     var serviceUUIDs = [SERVICE_UUID]; // default: [] => all
     var allowDuplicates = false; // default: false
