33f20a5bdd53389c233d8e463edde79cd59481e2
Arno Riedmann <arno.riedmann@students.fhv.at>
2017-06-07 16:24:26 +0200

    fucking css

--- public/javascript/calendar.js
+++ public/javascript/calendar.js
@@ -6,7 +6,8 @@ const APIURL = 'api/calendar/';
 function reloadCalendarEvents() {
     $.get(APIURL, function (data) {
         $('#calendar').empty();
-        $(`#calendar`).append(`<div id="calendarHead"><div id="calendarHeadH">Veranstaltungen</div></div>`);
+        $(`#calendar`).append(`<div id="calendarHead">Veranstaltungen</div>`);
+        $('#calendar').append(`<div id="calendarEvents"></div>`)
 
         for(var calendarEvent in data){
             if(calendarEvent == 3){
@@ -20,26 +21,17 @@ function reloadCalendarEvents() {
 function addCalendarEvent(calendarEvent) {
     var start = new Date(calendarEvent.start);
     var end = new Date(calendarEvent.end);
-    $('#calendar').append(`<div id="calendarEvent">
-                <div id="calendarDatetime">
-                    <div id="calendarDay">
-                        ` + ('0' + start.getDate()).slice(-2) + `
-                    </div>
-                    <div id="calendarMonth">
-                        ` + MONTHS[start.getMonth()] + `
-                    </div>
+    $('#calendarEvents').append(`
+            <div class="calendarDatetime">
+                <div class="calendarDay">${('0' + start.getDate()).slice(-2)}</div>
+                <div class="calendarMonth">${MONTHS[start.getMonth()]}</div>
+            </div>
+            <div class="calendarInformation">
+                <div class="calendarTitle">${calendarEvent.title}</div>
+                <div class="calendarTime">
+                    ${('0' + start.getHours()).slice(-2)  + `:` + ('0' + start.getMinutes()).slice(-2) + ` - ` + ('0' + end.getHours()).slice(-2) + `:` + ('0' + end.getMinutes()).slice(-2)}
                 </div>
-                <div id="calendarInformation">
-                    <div id="calendarTitle">
-                        ` + calendarEvent.title + `
-                    </div>
-                    <div id="calendarTime">
-                        ` + ('0' + start.getHours()).slice(-2)  + `:` + ('0' + start.getMinutes()).slice(-2) + ` - ` + ('0' + end.getHours()).slice(-2) + `:` + ('0' + end.getMinutes()).slice(-2) + `
-                    </div>
-                    <div id="calendarLocation">
-                        ` + calendarEvent.location + `
-                    </div>
-                </div>
+                <div class="calendarLocation">${calendarEvent.location}</div>
             </div>`);
 }
 
