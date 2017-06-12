e5e8cd170b7498a991968fdee04a1b6ff236273d
mondbaer <nfe3303@students.fhv.at>
2017-06-12 20:08:14 +0200

    bustable reset.. refresh whole table.. keep it simple

--- public/javascript/busstop.js
+++ public/javascript/busstop.js
@@ -1,23 +1,63 @@
-const BUSURLS = ["api/busstop/Koblach+Gasthaus+Harmonie", "api/busstop/Koblach+Dorfplatz"];
-var divTag;
-var count = 0;
-var busData = [];
-function callApi(dest_url) {
-    var result = null;
-    $.ajax({
-        url: dest_url,
-        type: 'get',
-        dataType: 'json',
-        async: false,
-        success: function (data) {
-            result = data;
+//const BUSURL = "api/busstop/Koblach+Gasthaus+Harmonie";
+const BUSURL = "api/busstop/Bregenz+Bahnhof";
+
+function loadBusStopInfo() {
+
+    $.getJSON(BUSURL, function (data) {
+        $("#busstop").empty();
+        $("#busstop").append(`<div id="busHead"><div id="busHeadH"> Abfahrtszeiten</div></div><div id="busHeadInformation"><table id="bustable">`);
+
+        var table = document.getElementById("bustable");
+        var tr = document.createElement('tr');
+
+        var td1 = document.createElement('td');
+        var td2 = document.createElement('td');
+        var td3 = document.createElement('td');
+        var td4 = document.createElement('td');
+        var td5 = document.createElement('td');
+        var text1 = document.createTextNode('Abfahrt\n in min');
+        var text2 = document.createTextNode('Linie');
+        var text3 = document.createTextNode('Ziel');
+        var text4 = document.createTextNode('Plan');
+        td1.appendChild(text1);
+        td2.appendChild(text2);
+        td3.appendChild(text3);
+        td4.appendChild(text4);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tr.appendChild(td4);
+        table.appendChild(tr);
+        //var items = data.response.venue.tips.groups[0].items;
+        //console.log(data);
+        //console.log(data.information);
+        document.getElementById("busHeadInformation").appendChild(table);
+        for (var busstop in data.information) { //console.log(data.information);
+            //console.log(data.information[busstop]);
+            if (busstop == 3) {
+                break;
+            }
+            addBus(data.information[busstop]);
         }
+        var table = document.createElement('table');
+
+        document.getElementById("busHeadInformation").appendChild(table);
+
     });
-    return result;
+
 }
-function createTable(){
-    $(`#busstop`).empty();
-    $(`#busstop`).append(`<div id="busHead"><div id="busHeadH"> Abfahrtszeiten</div></div><div id="busHeadInformation"><table id="bustable">`);
+function calcTimeDifToNow(time){
+    var timesplit = time.split(":");
+    var curr_date = new Date();
+    return (parseInt(timesplit[0]) * 60) + parseInt(timesplit[1]) - ((curr_date.getHours() * 60) + curr_date.getMinutes());
+}
+
+function addBus(data) {
+    var number = data.number;
+    var direction = data.direction;
+    var bustime = data.time;
+    var diffrence = calcTimeDifToNow(bustime);
+
     var table = document.getElementById("bustable");
     var tr = document.createElement('tr');
 
@@ -25,70 +65,19 @@ function createTable(){
     var td2 = document.createElement('td');
     var td3 = document.createElement('td');
     var td4 = document.createElement('td');
-    var td5 = document.createElement('td');
-    var text1 = document.createTextNode('Abfahrt\n in min');
-    var text2 = document.createTextNode('Linie');
-    var text3 = document.createTextNode('Ziel');
-    var text4 = document.createTextNode('Bushaltestelle');
-    var text5 = document.createTextNode('Plan');
+    var text1 = document.createTextNode(diffrence);
+    var text2 = document.createTextNode(number);
+    var text3 = document.createTextNode(direction);
+    var text4 = document.createTextNode(bustime);
     td1.appendChild(text1);
     td2.appendChild(text2);
     td3.appendChild(text3);
     td4.appendChild(text4);
-    td5.appendChild(text5);
     tr.appendChild(td1);
     tr.appendChild(td2);
     tr.appendChild(td3);
     tr.appendChild(td4);
-    tr.appendChild(td5);
     table.appendChild(tr);
-}
-
-function loadBusStopInfo() {
-
-    var arrayPos = 0;
-    for (var i = 0; i < BUSURLS.length; i++) {
-        var result = callApi(BUSURLS[i]);
-        //rework
-        for (var k = 0; k < result.information.length; k++) {
-            if (result.information[k].number != "") {
-                result.information[k]["busstop"] = result.busStopName;
-                busData[arrayPos] = result.information[k];
-                arrayPos++;
-            }
-        }
-    }
-    busData.sort(function (a, b) {
-        if (a.time < b.time)
-            return -1;
-        if (a.time > b.time)
-            return 1;
-        return 0;
-    });
-    var actualTime = new Date();
-    var displayedBuses = 0;
-    for (var busstop in busData) {
-        if (displayedBuses >= 3) {
-            break;
-        }
-        var splitTime = busData[busstop].time.split(":");
-        var bustime = (parseInt(splitTime[0]) * 60) + parseInt(splitTime[1]);
-        var diffrence = bustime - ((actualTime.getHours() * 60) + actualTime.getMinutes());
-        if (diffrence > 0) {
-            addBus(busData[busstop].number, busData[busstop].direction, busData[busstop].busstop, busData[busstop].time, diffrence);
-            displayedBuses++;
-        }
-
-
-    }
-    var table = document.createElement('table');
-
-    document.getElementById("busHeadInformation").appendChild(table);
-
-}
-
-function addBus(number, direction, busstop, depature, diffrence) {
-
 }
 
 
